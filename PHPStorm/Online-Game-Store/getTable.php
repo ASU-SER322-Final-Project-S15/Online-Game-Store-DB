88d3446f8c0e40bda9963e09b215f9fcb1fc5598
kole9273 <kole9273@gmail.com>
2015-04-21 16:51:11 -0700

    lots of cool stuff

--- PHPStorm/Online-Game-Store/getTable.php
+++ PHPStorm/Online-Game-Store/getTable.php
@@ -4,14 +4,15 @@
 $selectedTable = $_GET["type"];
 $userId = $_GET["userId"];
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "db4free.net:3306";
+$username = "sergaming";
+$password = "ASK KOLE";
+$database = "sergamedb";
 
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$database);
 
 // Check connection
 if ($conn->connect_error) {
@@ -22,12 +23,20 @@ if ($conn->connect_error) {
 $sql;
 
 
+/*
+echo '<script language="javascript">';
+echo 'alert("' . $selectedTable .   '")';
+echo '</script>'; */
 
 
-
-
-
-
+if($selectedTable == "Users")
+    $sql = "select * from User";
+else if ($selectedTable == "Games")
+    $sql = "select * from Game";
+else if ($selectedTable == "Devs")
+    $sql = "select * from Developer";
+else if ($selectedTable == "UserTH")
+    $sql = "select * from User where userId=" . $userId;
 
 
 
@@ -35,13 +44,81 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
+
+    $htmlTable = "<table align='center'>";
+
+    $htmlTable = getHeaders($htmlTable, $selectedTable);
+
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        $htmlTable = $htmlTable . "<tr>";
+
+        $htmlTable = getRow($htmlTable, $row, $selectedTable, $userId);
+
+
+        $htmlTable = $htmlTable . "</tr>";
+
+        //echo "id: " . $row["id"]. " - Title: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
+
+    $htmlTable = $htmlTable . "</table>";
+
+    echo $htmlTable;
+
 } else {
     echo "0 results";
 }
 $conn->close();
+
+
+
+function getRow($htmlTable, $row, $selectedTable, $userId){
+
+
+    if($selectedTable == "Users") {
+        $sql = "select * from User";
+    }
+    else if ($selectedTable == "Games") {
+        $htmlTable = $htmlTable . "<td>" . $row["id"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["Title"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["UPC"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["Developer"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["Publisher"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["Metascore"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["PublisherId"] . "</td>";
+        $htmlTable = $htmlTable . "<td>" . $row["ESRB"] . "</td>";
+    }
+    else if ($selectedTable == "Devs") {
+        $sql = "select * from Developer";
+    }
+    else if ($selectedTable == "UserTH") {
+        $sql = "select * from User where userId=" . $userId;
+    }
+
+
+//return "dlkfjdfjdf";
+    return $htmlTable;
+}
+
+function getHeaders($htmlTable, $selectedTable){
+
+    if($selectedTable == "Users") {
+        $sql = "select * from User";
+    }
+    else if ($selectedTable == "Games") {
+        $htmlTable = $htmlTable . "<th>id</th><th>Title</th><th>UPC</th><th>Developer</th><th>Publisher</th><th>Metascore</th><th>Publisher Id</th><th>ESRB</th>";
+
+    }
+    else if ($selectedTable == "Devs") {
+        $sql = "select * from Developer";
+    }
+    else if ($selectedTable == "UserTH") {
+        $sql = "select * from User where userId=" . $userId;
+    }
+
+
+    return $htmlTable;
+
+}
 
 
 ?>
